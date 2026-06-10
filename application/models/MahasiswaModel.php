e71d63f83d5d34ccaee614d9ecac3d4f19c1a0b5
Rizky <xynnaja701@gmail.com>
2026-06-10 16:51:46 +0800

    Update models/MahasiswaModel.php

--- application/models/MahasiswaModel.php
+++ application/models/MahasiswaModel.php
@@ -30,17 +30,17 @@ class MahasiswaModel extends CI_Model {
 	}
 
     public function checkAccount($data)
-    {
-        $this->db->where('mahasiswa_email', $data['email']);
-        $this->db->where('mahasiswa_password', sha1($data['password']));
-        $account = $this->db->get('mahasiswa', 1)->row_array();
+{
+    $this->db->where('mahasiswa_email', $data['email']);
+    $this->db->where('mahasiswa_password', sha1($data['password']));
+    $account = $this->db->get('mahasiswa', 1)->row_array();
 
-        if ($account) {
-            $this->session->set_userdata('user', $account);
-            return true;
-        }
-        else {
-            return false;
-        }
+    if ($account) {
+        $this->session->set_userdata('user', $account);
+        return true;
     }
+    else {
+        return false;
+    }
+}
 }
