0ac8101eeecaa297d9a873ddf962e7ede107717c
Rizky <xynnaja701@gmail.com>
2026-06-10 16:14:44 +0800

    Update models/ProdiModel.php

--- application/models/ProdiModel.php
+++ application/models/ProdiModel.php
@@ -1,43 +1,38 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class ProdiModel extends CI_Model {
+class ProdiModel extends CI_Model
+{
+    protected $table = 'prodi';
+    protected $pk    = 'prodi_id';
 
     public function getAll()
     {
         $this->db->select('prodi.*, fakultas.fakultas_name');
         $this->db->from('prodi');
-        $this->db->join('fakultas', 'fakultas.fakultas_id = prodi.fakultas_id');
-
+        $this->db->join('fakultas', 'fakultas.fakultas_id = prodi.fakultas_id', 'left');
         return $this->db->get()->result_array();
     }
 
     public function getById($id)
     {
-        return $this->db->get_where('prodi', [
-            'prodi_id' => $id
-        ])->row_array();
-    }
-
-    public function getFakultas()
-    {
-        return $this->db->get('fakultas')->result_array();
+        return $this->db->get_where($this->table, [$this->pk => $id])->row_array();
     }
 
     public function insert($data)
     {
-        return $this->db->insert('prodi', $data);
+        return $this->db->insert($this->table, $data);
     }
 
     public function update($id, $data)
     {
-        $this->db->where('prodi_id', $id);
-        return $this->db->update('prodi', $data);
+        $this->db->where($this->pk, $id);
+        return $this->db->update($this->table, $data);
     }
 
     public function delete($id)
     {
-        $this->db->where('prodi_id', $id);
-        return $this->db->delete('prodi');
+        $this->db->where($this->pk, $id);
+        return $this->db->delete($this->table);
     }
 }
